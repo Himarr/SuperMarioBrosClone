4d10ebe6f3f275540aecfd14d03094fa33b261d2
Himar <denizhimar@gmail.com>
2025-04-07 12:57:03 +0100

    .

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -14,41 +14,83 @@ public class Player : MonoBehaviour
     public float minSpeed;
     public float deceleration;
 
+    bool isMoving;
+    private int dir;
+
 
     void Start()
     {
-        
+        speed = 0;
     }
 
     
     void Update()
     {
-        
+        if (Input.GetKeyUp(KeyCode.D) || Input.GetKeyUp(KeyCode.A)) { isMoving = false; }
+        HandleMovement();
     }
 
     private void FixedUpdate()
     {
-        HandleMovement();
+        
+        Debug.Log(dir);
     }
 
     private void HandleMovement()
     {
-        if (Input.GetKeyDown(KeyCode.D) && speed == 0)
+        if (Input.GetKeyDown(KeyCode.D) && speed <= 0)
             // Velocidad inicial
         {
             speed = minSpeed;
+            dir = 1;
         }
-        if (Input.GetKey(KeyCode.D))
+        if (Input.GetKey(KeyCode.D) && dir == 1)
             // Añadir aceleración
         {
+            isMoving = true;
+
             if (speed <= maxSpeed)
             {
-                speed += acceleration;
+                speed += acceleration * Time.deltaTime;
             }
-            
-
-            this.transform.position += new Vector3(speed, 0) * Time.deltaTime;
+            if (speed <= 0 && dir == -1)
+            {
+                speed = minSpeed;
+                dir = 1;
+            }
         }
-        
+
+        if (Input.GetKeyDown(KeyCode.A) && speed <= 0)
+        // Velocidad inicial
+        {
+            speed = minSpeed;
+            dir = -1;
+        }
+        if (Input.GetKey(KeyCode.A) && dir == -1)
+        // Añadir aceleración
+        {
+            isMoving = true;
+
+            if (speed <= maxSpeed)
+            {
+                speed += acceleration * Time.deltaTime;
+            }
+            if (speed <= 0 && dir == 1) 
+            { 
+                speed = minSpeed; 
+                dir = -1;
+            }
+        }
+
+
+        if (speed > 0 && !isMoving)
+        {
+            speed -= deceleration * Time.deltaTime;
+        }
+
+        if (speed < 0) { speed = 0; }
+
+        // Mover
+        this.transform.position += new Vector3(speed, 0) * Time.deltaTime * dir;
     }
 }
