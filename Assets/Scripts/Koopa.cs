b292131b82f7a80fd3f90f95985ac9b958e9edf4
PSkyland <pablom.skyland@gmail.com>
2025-05-15 13:34:49 +0100

    Arreglos en koopa y bloques
    Se arregló aun más el koopa, se hicieron variantes del question block para overworld y underground con sus animaciones, y se han comenzado a colocar los bloques por el escenario.

--- Assets/Scripts/Koopa.cs
+++ Assets/Scripts/Koopa.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Koopa : MonoBehaviour
@@ -10,6 +11,8 @@ public class Koopa : MonoBehaviour
     Rigidbody2D rb2D;
     BoxCollider2D boxCollider;
     Player player;
+
+    bool canMove = true;
 
     private void Start()
     {
@@ -22,32 +25,16 @@ public class Koopa : MonoBehaviour
     void Update()
     {
         //Movimiento del koopa
-        gameObject.transform.Translate(koopaVelocity * Time.deltaTime, 0, 0);
-
-    }
-
-    private void OnTriggerEnter2D(Collider2D collision)
-    {
-        //Rebote en la pared
-        if (collision.gameObject.CompareTag("Block") || collision.gameObject.CompareTag("Enemy") || collision.gameObject.CompareTag("Breakable"))
+        if (canMove)
         {
-            koopaVelocity = koopaVelocity * -1;
+            gameObject.transform.Translate(koopaVelocity * Time.deltaTime, 0, 0);
         }
-
-        if (gameObject.GetComponent<SpriteRenderer>().flipX == false)
-        {
-            gameObject.GetComponent<SpriteRenderer>().flipX = true;
-        }
-        else
-        {
-            gameObject.GetComponent<SpriteRenderer>().flipX = false;
-        }
-
 
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
+        //Determina la direccion a la que se lanza el caparazon
         float koopaCenter = boxCollider.bounds.center.x;
         float marioCenter = player.col.bounds.center.x;
 
@@ -70,6 +57,7 @@ public class Koopa : MonoBehaviour
             }
         }
 
+        //Koopa se mete en el caparazon cuando Mario le salta encima
         if (player != null && player.jumpForce < 0 && collision.gameObject.CompareTag("Player"))
         {
             Debug.Log("Koopa caparazon");
@@ -81,6 +69,44 @@ public class Koopa : MonoBehaviour
 
             ThrowShell();
         }
+
+        //Muerte del koopa
+        if (collision.gameObject.CompareTag("KoopaInShell"))
+        {
+            KoopaDead();
+        }
+
+        //Koopa mata a Mario
+        if (collision.gameObject.CompareTag("Player") && (koopaVelocity == 6 || koopaVelocity == -6))
+        {
+            Debug.Log("Mario muere :(");
+            player.onHit();
+        }
+
+        //Caparazon reobta en las paredes
+        if (collision.gameObject.CompareTag("Block") || collision.gameObject.CompareTag("Breakable"))
+        {
+            koopaVelocity = koopaVelocity * -1;
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        //Rebote en la pared
+        if (collision.gameObject.CompareTag("Enemy"))
+        {
+            koopaVelocity = koopaVelocity * -1;
+        }
+
+        if (gameObject.GetComponent<SpriteRenderer>().flipX == false)
+        {
+            gameObject.GetComponent<SpriteRenderer>().flipX = true;
+        }
+        else
+        {
+            gameObject.GetComponent<SpriteRenderer>().flipX = false;
+        }
+
     }
 
     public void koopaInShell()
@@ -100,5 +126,15 @@ public class Koopa : MonoBehaviour
         }
     }
 
-    
+    public void KoopaDead()
+    {
+        rb2D.AddForce(new Vector2(12 * Time.deltaTime, 4 * Time.deltaTime));
+        gameObject.GetComponent<Animator>().enabled = false;
+        canMove = false;
+        gameObject.layer = LayerMask.NameToLayer("NoColission");
+        gameObject.GetComponent<SpriteRenderer>().flipY = true;
+
+        Destroy(gameObject, 1f);
+    }
+
 }
