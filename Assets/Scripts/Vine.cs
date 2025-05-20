0aece7ff380cd77183fc8dee71e5a9537c97ed62
PSkyland <pablom.skyland@gmail.com>
2025-05-20 11:39:56 +0100

    Enredadera
    La enredadera ya crece cuando el bloque es golpeado, falta que Mario la pueda escalar

---
+++ Assets/Scripts/Vine.cs
@@ -0,0 +1,92 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using static UnityEditor.Experimental.GraphView.GraphView;
+
+public class Vine : MonoBehaviour
+{
+
+    public float vineVelocity;
+    public float initialPositionX;
+    public float timer;
+
+    public GameObject vinePiece;
+    public GameObject vinePiece1;
+    public GameObject vinePiece2;
+    public GameObject vinePiece3;
+    public GameObject vinePiece4;
+    public GameObject vinePiece5;
+
+    BoxCollider2D col;
+
+
+    void Start()
+    {
+        initialPositionX = transform.position.x;
+        BoxCollider2D col = new BoxCollider2D();
+    }
+
+    
+    void Update()
+    {
+
+        
+
+    }
+
+    public IEnumerator VineMovement()
+    {
+        
+        
+
+        
+        while(timer < 3.5){
+            gameObject.transform.Translate(0, vineVelocity * Time.deltaTime, 0);
+
+            if (timer > 0.5)
+            {
+                vinePiece.transform.parent = gameObject.transform;
+            }
+            if (timer > 1)
+            {
+                vinePiece1.transform.parent = gameObject.transform;
+            }
+            if (timer > 1.5)
+            {
+                vinePiece2.transform.parent = gameObject.transform;
+            }
+            if (timer > 2)
+            {
+                vinePiece3.transform.parent = gameObject.transform;
+            }
+            if (timer > 2.5)
+            {
+                vinePiece4.transform.parent = gameObject.transform;
+            }
+            if (timer > 3)
+            {
+                vinePiece5.transform.parent = gameObject.transform;
+            }
+            if (timer > 3.5)
+            {
+                vineVelocity = 0;
+            }
+
+            timer += Time.deltaTime;
+            
+             yield return null;
+        }
+       
+
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Player player = collision.gameObject.GetComponent<Player>();
+        if (player != null && collision.CompareTag("Player"))
+        {
+
+            StartCoroutine(VineMovement());
+            
+        }
+    }
+}
