5b56ffca8b9588bc8b90deeebf984108e32c730e
Lucy <luciahs2006@gmail.com>
2025-05-22 11:22:43 +0100

    Bandera avances
    Colliders establecidos
    Mario contacto con palo de bandera, baja y se reproduce la animación
    Falta: flip, que la bandera baje, y el movimiento de Mario hacia el castillo

--- Assets/Scripts/Flag.cs
+++ Assets/Scripts/Flag.cs
@@ -1,15 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Flag : MonoBehaviour
 {
     Player player;
     BoxCollider2D col;
-    Animation anim;
+    Animator anim;
 
     public float moveSpeed;
-    public float time;
+    public float tiempo;
+    public float coords = -3.5f;
+    
+
 
 
 
@@ -17,19 +21,42 @@ public class Flag : MonoBehaviour
     {
         player = GameObject.Find("Mario").GetComponent<Player>();
         col = GetComponent<BoxCollider2D>();
+        anim = player.GetComponent<Animator>();
     }
 
     public void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.CompareTag("Player"))
-        { 
-          //transform.position = collision.transform.position;
+        {
+            player.canMove = false;
+            anim.SetBool("isFlagDown", true); 
+            StartCoroutine(MarioDown());
+
         }
     }
+
+    IEnumerator MarioDown()
+    {
+        Vector3 inicio = player.transform.position;
+        Vector3 fin = new Vector3(inicio.x, coords, inicio.z);
+        float elapsed = 0;
+        while (elapsed < tiempo)
+        {
+            
+            player.transform.position = Vector3.Lerp(inicio, fin, elapsed / tiempo);
+            elapsed += Time.deltaTime;
+            yield return null;
+        }
+        yield return  null;
+
+
+    }
+
+    
 
 
     void Update()
     {
-        
+
     }
 }
