d4fa337651a8591f0a987ab92a2cf9e75a7de5a1
Lucy <luciahs2006@gmail.com>
2025-05-27 10:26:32 +0100

    Tuberia Horizontal
    Terminada

--- Assets/Scripts/PipeHorizontal.cs
+++ Assets/Scripts/PipeHorizontal.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Timers;
 using Unity.Collections.LowLevel.Unsafe;
 using UnityEngine;
 
@@ -11,6 +12,7 @@ public class PipeHorizontal : MonoBehaviour
     public AudioClip pipeDown;
     public AudioSource audioSource;
     bool trigger = false;
+    float tiempo = 1 ;
 
     void Start()
     {
@@ -18,16 +20,53 @@ public class PipeHorizontal : MonoBehaviour
         col = GetComponent<BoxCollider2D>();
     }
 
-
-    private void OnTriggerStay2D(Collider2D collision)
+    private void OnTriggerEnter2D(Collider2D collision)
     {
 
-        if (Input.GetKeyDown(KeyCode.D) && trigger == false)
+        if (Input.GetKey(KeyCode.D) && trigger == false &&(player.dir == 1))
         {
+            StartCoroutine(AnimacionPipeR());
+            AudioSource.PlayClipAtPoint(pipeDown, gameObject.transform.position);
+        }
+
+        if (Input.GetKey(KeyCode.A) && trigger == false && (player.dir == -1))
+        {
+            StartCoroutine(AnimacionPipeL());
+            AudioSource.PlayClipAtPoint(pipeDown, gameObject.transform.position);
+        }
+
+        IEnumerator AnimacionPipeR()
+        {
+            Debug.Log("comienza corrutina");
+            player.canMove = false;  
             Vector3 inicio = player.transform.position;
-            Vector3 fin = new Vector3(inicio.x += 2f, inicio.y, inicio.z);
+            Vector3 fin = new Vector3(inicio.x + 2f, inicio.y, inicio.z);
+            float elapsed = 0;
+            while (elapsed < tiempo)
+            {
+                player.transform.position = Vector3.Lerp(inicio, fin, elapsed / (tiempo));
+                elapsed += Time.deltaTime;
+                yield return null;
+            }
 
+            yield return null;
+        }
 
+        IEnumerator AnimacionPipeL()
+        {
+            Debug.Log("comienza corrutina");
+            player.canMove = false;
+            Vector3 inicio = player.transform.position;
+            Vector3 fin = new Vector3(inicio.x + -2f, inicio.y, inicio.z);
+            float elapsed = 0;
+            while (elapsed < tiempo)
+            {
+                player.transform.position = Vector3.Lerp(inicio, fin, elapsed / (tiempo));
+                elapsed += Time.deltaTime;
+                yield return null;
+            }
+
+            yield return null;
         }
     }
 }
