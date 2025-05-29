07a6c499c5a4a8bfb20ca203815945a950adb63c
kazrrefour <laurabm2306@gmail.com>
2025-05-29 17:08:42 +0100

    Cambio de escena y ajuste de vidas
    Cambio de escena del 1-1 al Load y ajuste en la cantidad de vidas

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -8,7 +8,7 @@ public class GameManager : MonoBehaviour
     public static GameManager Instance;
 
     int score;
-    int lives = 2;
+    int lives = 3;
     int coins = 0;
     string playerState;
     float timer = 400;
